6f38bce3f05c6a687a97f41a838adf2d2c786128
Lalitha-Iyer <lalithaiyer@Lalithas-MBP.vensav.com>
2017-08-06 18:39:53 -0700

    fetch from books api

--- src/App.js
+++ src/App.js
@@ -13,59 +13,26 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: true,
-    books : [
-      {
-        title: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        url : "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        state: "Currently Reading"
-      },
-      {
-        title: "Ender's Game",
-        author: "Orson Scott Card",
-        url: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        state: "Currently Reading"
-
-      },
-      {
-        title: "1776",
-        author: "David McCullough" ,
-        url: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-        state: "Want to Read"
-      },
-      {
-        title: "Harry Potter and the Sorcerer's Stone",
-        author: "J.K. Rowling" ,
-        url: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-        state: "Want to Read"        
-      },
-      {
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        url: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-        state: "Read"
-      },     
-       {
-        title: "Oh, the Places You'll Go!",
-        author: "Seuss",
-        url: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-        state: "Read"
-      }
-    ]  
+    books:[]
   }
-  
-  updateBookState = (value,title) => {
-    this.setState((prevState)=> {
-         prevState.books.map((book)=>{
-          if(title === book.title){
-            console.log(event)
-            book.state = value
-          } 
-        })
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.state.books  = books;
+      console.log(JSON.stringify(books[0]))
     })
-   console.log(JSON.stringify(this.state.books))   
-  }    
-  
+  }
+
+  updateBookState = (value, title) => {
+    this.setState((prevState) => {
+      prevState.books.map((book) => {
+        if (title === book.title) {
+          book.shelf = value
+        }
+      })
+    })
+  }
+
 
   render() {
     return (
@@ -83,30 +50,30 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
-                
+                <input type="text" placeholder="Search by title or author" />
+
               </div>
             </div>
             <div className="search-books-results">
-                <BookShelf books={this.state.books} update={this.updateBookState} />
+              <BookShelf books={this.state.books} update={this.updateBookState} />
             </div>
           </div>
         ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <BookShelf books={this.state.books} update={this.updateBookState} title="Currently Reading"/>
-              <BookShelf books={this.state.books} update={this.updateBookState} title="Want to Read"/>
-              <BookShelf books={this.state.books} update={this.updateBookState}  title="Read"/>
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <BookShelf books={this.state.books} update={this.updateBookState} shelf="currentlyReading" title="Currently Reading" />
+                <BookShelf books={this.state.books} update={this.updateBookState} shelf="wantToRead" title="Want to Read" />
+                <BookShelf books={this.state.books} update={this.updateBookState} shelf="read" title="Read" />
 
+              </div>
+              <div className="open-search">
+                <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              </div>
             </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+          )}
       </div>
     )
   }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -5,34 +5,34 @@ class BookShelf extends React.Component {
 
   render() {
     const { books } = this.props
-    const {title} = this.props
+    const { title, shelf } = this.props
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
             {
-              books.filter((book)=> title? book.state === title : true)
-              .map((book, index) => (
-                <li key={index}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.url})` }}></div>
-                      <div className="book-shelf-changer">
-                        <select onChange={(event)=>this.props.update(event.target.value, book.title)}>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="Currently Reading"  selected={book.state === "Currently Reading"}>Currently Reading</option>
-                          <option value="Want to Read" selected={book.state === "Want to Read"}>Want to Read</option>
-                          <option value="Read" selected={book.state === "Read"}>Read</option>
-                          <option value="None" selected={book.state === "none"}>None</option>
-                        </select>
+              books.filter((book) => title ? book.shelf === shelf : true)
+                .map((book, index) => (
+                  <li key={index}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        <div className="book-shelf-changer">
+                          <select onChange={(event) => this.props.update(event.target.value, book.title)}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading" >Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read" >Read</option>
+                            <option value="None" >None</option>
+                          </select>
+                        </div>
                       </div>
+                      <div className="book-title">book.title</div>
+                      <div className="book-authors">book.author</div>
                     </div>
-                    <div className="book-title">book.title</div>
-                    <div className="book-authors">book.author</div>
-                  </div>
-                </li>
-              ))
+                  </li>
+                ))
             }
           </ol>
         </div>
